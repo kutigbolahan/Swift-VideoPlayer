1fdd7a79a1a695af758a625459372130b696f126
Kuti Gbolahan <kuti.gbolahan@thegiggroupng.com>
2022-01-26 10:55:01 +0100

    first commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by GB on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
